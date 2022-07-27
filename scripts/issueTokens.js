41b78c11c3b9741ecafa054cd2d6e6822d8190fc
MKen212 <46496040+MKen212@users.noreply.github.com>
2022-07-27 18:55:33 +0200

    Create the issueTokens.js script

---
+++ scripts/issueTokens.js
@@ -0,0 +1,16 @@
+/* global artifacts contract */
+const TokenFarm = artifacts.require("TokenFarm");
+
+module.exports = async function(callback) {
+  // Get deployed tokenFarm contract
+  const tokenFarm = await TokenFarm.deployed();
+
+  // Issue reward tokens
+  await tokenFarm.issueTokens();
+
+  // Report to console
+  console.log("Reward Tokens Issued!!");
+
+  // Run callback
+  callback();
+};
